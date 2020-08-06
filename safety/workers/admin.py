5bd82aaae725641a5e21c77c9abb258f903840f2
SUPAWIT <supawit60.kli@gmail.com>
2020-08-06 14:20:52 +0700

    add test db

--- safety/workers/admin.py
+++ safety/workers/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Worker
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'is_available',
+        'primary_phone',
+        'secondary_phone',
+        'address',
+    )
+    list_filter = (
+        'is_available',
+    )
--- safety/workers/tests.py
+++ safety/workers/tests.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.test import TestCase
 
 from .models import Worker
+from .admin import WorkerAdmin
 
 
 class TestWorker(TestCase):
@@ -30,3 +32,25 @@ class TestWorker(TestCase):
         self.assertEqual(worker.primary_phone, primary_phone)
         self.assertEqual(worker.secondary_phone, secondary_phone)
         self.assertEqual(worker.address, address)
+
+
+class WorkerAdminTest(TestCase):
+    def test_admin_should_be_registered(self):
+        self.assertTrue(isinstance(admin.site._registry[Worker], WorkerAdmin))
+
+    def test_admin_should_set_list_display(self):
+        excepted = (
+            'first_name',
+            'last_name',
+            'is_available',
+            'primary_phone',
+            'secondary_phone',
+            'address',
+        )
+        self.assertEqual(WorkerAdmin.list_display, excepted)
+
+    def test_admin_should_set_list_filter(self):
+        excepted = (
+            'is_available',
+        )
+        self.assertEqual(WorkerAdmin.list_filter, excepted)
