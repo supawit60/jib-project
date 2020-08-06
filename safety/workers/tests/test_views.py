a94d29e367257962bf440953f18132e70ad7d4c3
SUPAWIT <supawit60.kli@gmail.com>
2020-08-06 15:40:47 +0700

    create test view in for loop

--- safety/workers/tests/test_views.py
+++ safety/workers/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from ..models import Worker
 
 
 class TestWorkerListView(TestCase):
@@ -6,3 +7,29 @@ class TestWorkerListView(TestCase):
         response = self.client.get('/workers/')
         # print(dir(response))
         self.assertEqual(response.status_code, 200)
+
+    def test_view_should_render_list_of_worker_names(self):
+        # Given
+        Worker.objects.create(
+            first_name='Supawit',
+            last_name='Klinhom',
+            is_available=True,
+            primary_phone='08888888',
+            secondary_phone='09999999',
+            address='Bangkok',
+        )
+        Worker.objects.create(
+            first_name='Test',
+            last_name='Klinhom',
+            is_available=False,
+            primary_phone='08888888',
+            secondary_phone='09999999',
+            address='Thailand',
+        )
+
+        # When
+        response = self.client.get('/workers/')
+
+        # Then
+        self.assertContains(response, '<li>Supawit</li>')
+        self.assertContains(response, '<li>Test</li>')
--- safety/workers/views.py
+++ safety/workers/views.py
@@ -1,8 +1,23 @@
 from django.views import View
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Worker
 
 
 # class base view
 class WorkerListView(View):
     def get(self, request):
-        return HttpResponse()
+        workers = Worker.objects.all()
+        print(workers)
+        # html = ''
+        # for worker in workers:
+        #     html += f'<li>{worker.first_name}</li>'
+
+        worker_list = []
+        for worker in workers:
+            worker_list.append(worker.first_name)
+
+        return render(request, 'worker_list.html', {
+            'workers': worker_list
+        })
