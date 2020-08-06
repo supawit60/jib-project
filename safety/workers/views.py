b07a4532dad4ab1e69c91b02f8ac1ccb31f38756
SUPAWIT <supawit60.kli@gmail.com>
2020-08-06 15:01:46 +0700

    create test view 200

--- safety/workers/views.py
+++ safety/workers/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+
+# class base view
+class WorkerListView(View):
+    def get(self, request):
+        return HttpResponse()
