afd920283817f691935453545f595267508275b5
SUPAWIT <supawit60.kli@gmail.com>
2020-08-08 15:04:35 +0700

    Add setup, taerDown

--- safety/certificates/tests/test_model.py
+++ safety/certificates/tests/test_model.py
@@ -4,6 +4,12 @@ from ..models import Certificate
 
 
 class TestWorker(TestCase):
+    def setup(self):
+        pass
+
+    def tearDown(self):
+        return super().tearDown()
+
     def test_certificate_should_have_defind_fields(self):
         # Given
         name = 'Django Certificate By ODDS'
